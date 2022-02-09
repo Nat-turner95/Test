ba44899c687282b7e736b7b48621d1a2f436e54c
Nat-turner95 <ykmc95@gmail.com>
2022-02-09 14:38:55 +0000

    testing commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int a=0; a<5; a++){
+            System.out.println("hello world");
+        }
+        System.out.println("End");
+    }
+}
